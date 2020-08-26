9cde97fedacdb22428c0eefd6a7a1e5eec9eb0f1
Roberto Bocio Melo <bociomelors@gmail.com>
2020-08-26 17:04:22 -0400

    Updates

---
+++ precache-manifest.83b1b35f209d9e4703c0b2c605066725.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ee97495fa55d3e979c7326aca52bf4",
+    "url": "/game-three-in-stripe/index.html"
+  },
+  {
+    "revision": "3d2deb449275de57f807",
+    "url": "/game-three-in-stripe/static/css/main.00cd9408.chunk.css"
+  },
+  {
+    "revision": "efac28857b8c4e8818dc",
+    "url": "/game-three-in-stripe/static/js/2.a5f385e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-three-in-stripe/static/js/2.a5f385e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d2deb449275de57f807",
+    "url": "/game-three-in-stripe/static/js/main.c613c637.chunk.js"
+  },
+  {
+    "revision": "af6019bcf85bfe5cbece",
+    "url": "/game-three-in-stripe/static/js/runtime-main.1b89542a.js"
+  }
+]);
